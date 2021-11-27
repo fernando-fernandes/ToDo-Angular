a970f817fc49b7a5ca96bc79f2eaf46ce59986c3
Fernando <fernando.85@outlook.com>
2021-11-27 17:05:42 -0300

    ReactiveFormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-angular';
+  public todos: any[] = []
+  public title: string = 'Minhas tarefas'
+  public form: FormGroup
+
+  constructor( private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    })
+
+    this.todos.push(new Todo(1, "Colocar ração para a Nina", false))
+    this.todos.push(new Todo(2, 'Fechar as janelas', true))
+    this.todos.push(new Todo(3, 'Jogar o lixo', false))
+
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if(index !== -1) {
+      this.todos.splice(index, 1)
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false
+  }
 }
